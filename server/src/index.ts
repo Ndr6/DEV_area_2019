6ba08396ddbb2314197f98d9c00f0114411db6fa
clapota <abel.vinuela-hamoudi@epitech.eu>
2020-01-10 19:35:40 +0100

    [+] User creation, database configuration

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,21 @@
 import express from "express";
+import {createConnection} from "typeorm";
+import bodyParser from "body-parser";
+import userRoute from "./Routes/User";
 const port = process.env.PORT || 8080;
 const app = express();
 
+
+app.use(bodyParser.json());
 app.get('/', (req, res) => {
     res.send('Server working with typescript');
 });
 
-app.listen(port, () => {
-    console.log('Server listening on port ' + port);
+app.use('/user', userRoute);
+
+createConnection().then(_ => {
+    app.listen(port, () => {
+        console.log('Server listening on port ' + port);
+    });
 });
+
