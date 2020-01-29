86c49eff54096547025f8dffed1e89c135273105
Sébastien Bernard <sebastien.bernard@epitech.eu>
2020-01-29 17:09:30 +0100

    Added dashboard codebase (routing, auth and jwt)

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,43 @@
-import express from "express";
+import express from 'express';
+import bodyParser from 'express';
+//import routes from './routes'
 
-var port = 3000;
-if (process.env.PORT)
-    port = process.env.PORT;
+// Constants
+const PORT = (process.env.PORT == undefined ? 36969 : process.env.PORT);
+const HOST = "0.0.0.0";
+
+const back_version = 0.1;
+const api_version = 0;
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello Area server!'));
-app.listen(port, () => console.log(`Area server listening on port ${port}!`));
+// DB Connection
+//TODO: Connect to DB
+
+// Middlewares
+app.use((req, res, next) => {
+    //TODO: Restrict the CORS access
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+//app.use(routes);
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('<h1>Area back-end.</h1>\nIf you see this you went the wrong way\n');
+});
+
+app.get('/about', (req, res) => {
+    res.status(200).json({
+        description: "Area back-end",
+        authors: "Sébastien Bernard and Hugo Bouleux",
+        version: back_version,
+        api_version: api_version
+    });
+});
+
+app.listen(PORT, HOST);
+console.log(`[Serv] Init > Running on http://${HOST}:${PORT}`);
