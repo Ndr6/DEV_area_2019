7360fc1586bf3d7dc58a3a972cf94eac2faf7f7b
Sébastien Bernard <sebastien.bernard@epitech.eu>
2020-01-29 14:37:11 +0100

    Initialized server-dev branch, and deploy path

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,10 @@
 import express from "express";
 
-var port = 3000
+var port = 3000;
 if (process.env.PORT)
     port = process.env.PORT;
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get('/', (req, res) => res.send('Hello Area server!'));
+app.listen(port, () => console.log(`Area server listening on port ${port}!`));
