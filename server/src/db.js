8a7a7038ed731e3661d30969179638d11e2ffd2b
Sébastien Bernard <sebastien.bernard@epitech.eu>
2020-02-07 11:19:01 +0100

    Added db and server services in docker-compose, fixed db bug in server

--- server/src/db.js
+++ server/src/db.js
@@ -2,7 +2,7 @@
 //Sébastien Bernard
 import mongodb from "mongodb";
 
-const DB_HOST = (process.env.DB_HOST == undefined ? "localhost" : process.env.DB_PORT);
+const DB_HOST = (process.env.DB_HOST == undefined ? "localhost" : process.env.DB_HOST);
 const DB_PORT = (process.env.DB_PORT == undefined ? "27017" : process.env.DB_PORT);
 const DB_NAME = (process.env.DB_NAME == undefined ? "area" : process.env.DB_NAME);
 let url = 'mongodb://' + DB_HOST + ':' + DB_PORT;
@@ -13,6 +13,7 @@ let client = new mongodb.MongoClient(url);
 client.connect(function (err) {
     if (err != null) {
         console.error("[DB  ] Init > Failed to connect, error below");
+        console.error("[DB  ] Init > DB url: " + url);
         console.error(err);
         process.exit();
     }
