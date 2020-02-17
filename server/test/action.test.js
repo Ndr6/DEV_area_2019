bb9b6263f370f8064eaa690ce649586b3ab519da
Hugo Bouleux <hugo.bouleux@epitech.eu>
2020-02-17 13:47:37 +0100

    [+]Server: Action: add new unit test for actionUserIntraEpitechNote

--- server/test/action.test.js
+++ server/test/action.test.js
@@ -3,14 +3,17 @@ import actionIntraEpitechNote from "../src/action"
 
 describe('Action', function () {
     describe('Call API and check condition', function () {
-        it('should check from the user auto-login token retrieved if there is a new note', function () {
+        it('should check from the user auto-login token retrieved if there is a new note from fake user', function () {
             let fakeUserAutoLoginToken = "https://intra.epitech.eu/auth-dac602d44f86d0927b3f462";
-            let validUserAutoLoginToken = "https://intra.epitech.eu/auth-dac602d44f86d0927b3f4624020b8cea7df379c0";
 
             let returnValueFake = actionIntraEpitechNote(fakeUserAutoLoginToken);
-            let returnValue = actionIntraEpitechNote(validUserAutoLoginToken);
             assert.equal(returnValueFake, "KO");
+        });
+        it('should check from the user auto-login retrieved if there is a new note from valid user', function () {
+            let validUserAutoLoginToken = "https://intra.epitech.eu/" + process.env.AUTO_LOGIN_TOKEN_INTRA;
+
+            let returnValue = actionIntraEpitechNote(validUserAutoLoginToken);
             assert.equal(returnValue, "OK: Passed");
-        })
-    })
+        });
+    });
 });
