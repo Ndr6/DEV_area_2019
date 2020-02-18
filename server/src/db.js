5772a10022aed5fb6d39e5f40d1b34deb35f4994
Sébastien Bernard <sebastien.bernard@epitech.eu>
2020-02-18 13:47:12 +0100

    Added more db wrapping

--- server/src/db.js
+++ server/src/db.js
@@ -2,7 +2,7 @@
 //Sébastien Bernard
 import mongodb from "mongodb";
 
-const DB_HOST = (process.env.DB_HOST == undefined ? "localhost" : process.env.DB_HOST);
+const DB_HOST = (process.env.DB_HOST == undefined ? "127.0.0.1" : process.env.DB_HOST);
 const DB_PORT = (process.env.DB_PORT == undefined ? "27017" : process.env.DB_PORT);
 const DB_NAME = (process.env.DB_NAME == undefined ? "area" : process.env.DB_NAME);
 let url = 'mongodb://' + DB_HOST + ':' + DB_PORT;
@@ -10,29 +10,36 @@ if (process.env.MONGODB_URI != undefined)
     url = process.env.MONGODB_URI;
 
 class db {
-    #client = undefined;
+    client = undefined;
+    #database = undefined;
     constructor() {
-        this.#client = new mongodb.MongoClient(url);
-        this.#client.connect(function (err) {
+        this.client = new mongodb.MongoClient(url);
+        console.log("[DB  ] Init > DB url: " + url);
+        this.client.connect(function (err, client) {
             if (err != null) {
                 console.error("[DB  ] Init > Failed to connect, error below");
                 console.error("[DB  ] Init > DB url: " + url);
                 console.error(err);
                 process.exit();
             }
-            console.log("[DB  ] Init > Connected to DB server");
         });
     }
 
-    test_db() {
-        if (this.#client.isConnected() == false)
-            return false;
-        return true
+    regen() {
+        this.#database = this.client.db(DB_NAME);
     }
 
-    close_db() {
-        if (this.#client.isConnected())
-            this.#client.close();
+    get() {
+        return this.#database;
+    }
+
+    test_connection() {
+        return this.client.isConnected();
+    }
+
+    close_connection(force, callback) {
+        if (this.client.isConnected())
+            this.client.close(force, callback);
     }
 }
 
