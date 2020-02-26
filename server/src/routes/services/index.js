a83ecd6d3b783f4e2f8fb7180aac5abcaaed75e9
Sébastien Bernard <sebastien.bernard@epitech.eu>
2020-02-26 16:30:39 +0100

    Removed SMTP service, added description list of all services, actions and reactions

--- server/src/routes/services/index.js
+++ server/src/routes/services/index.js
@@ -4,7 +4,6 @@ import { Router } from "express";
 import intraService from "./intra";
 import googleService from "./google";
 import twitterService from "./twitter";
-import smtpService from "./smtp";
 
 //Actions
 //import checkFeed from 'action/rssFeed';
@@ -23,7 +22,6 @@ const routes = Router();
 routes.use("/intra", intraService);
 routes.use("/google", googleService);
 routes.use("/twitter", twitterService);
-routes.use("/smtp", smtpService);
 
 // Actions
 //routes.use('/rss', checkFeed);
@@ -33,5 +31,64 @@ routes.use("/smtp", smtpService);
 // Reactions
 //routes.use('/services/discord', discordWebhook);
 //routes.use('/services/action/mail', sendMail);
+
+routes.get('/list', (req, res) => {
+    let list = {
+        success: true,
+        services: {
+            intra: {
+                description: "The Épitech intranet, used by Épitech students to access courses and grades",
+                version: 1,
+                parameters: {
+                    token: "String - The autologin token, starts with 'auth-' and is 45 characters long"
+                }
+            },
+            google: {
+                description: "Google account, might be used to get calendar, contact, or mail information",
+                version: 1,
+                parameters: {
+                    token: "String - The OAuth access token, is shorter than 2048 character"
+                }
+            },
+            twitter: {
+                description: "Twitter account, might be used to get tweets, or to tweet",
+                version: 1,
+                parameters: {
+                    token: "String - The OAuth access token"
+                }
+            }
+        },
+        actions: {
+            placeholder: {
+                description: "This is not a real service, and is only used as an example for this list format",
+                version: -1,
+                settings: {
+                    checkInterval: 3600,
+                    user_configuration: {
+                        server: "String - The server to check for banana received",
+                        banana_nb: "Int - The minimum number of bananas to receive to trigger reactions",
+                        odd_banana: "Bool - Trigger reaction if receiving odd number of bananas"
+                    }
+                }
+            }
+        },
+        reactions: {
+            placeholder: {
+                description: "This is not a real service, and is only used as an example for this list format",
+                version: -1,
+                settings: {
+                    triggerCooldown: 3600,
+                    user_configuration: {
+                        server: "String - The server to reach",
+                        bananas: "Int - The number of bananas to send",
+                        recipient: "String - Username of the banana recipient"
+                    }
+                }
+            }
+        }
+    }
+    res.status(200).json(list);
+    return;
+});
 
 export default routes;
