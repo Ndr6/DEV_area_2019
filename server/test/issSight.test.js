4b86720b9639a1177675c3fb160995b87470be85
William <dev@williamblu.me>
2020-02-27 16:25:36 +0100

    [*] Adding a wait between requests in issSight test

--- server/test/issSight.test.js
+++ server/test/issSight.test.js
@@ -6,14 +6,15 @@ const wait = ms => new Promise(resolve => setTimeout(resolve, ms));
 describe('IssSightingOverLocation', function () {
     describe('Call API and check condition', function () {
         it('should return an error from a unvalid city name', async function () {
+            await wait(1250);
+
             let fakeCityName = "Oui";
 
             let returnValueFake = await issSightingOverLocation(fakeCityName);
             assert.equal(returnValueFake, "KO: Wrong location, enter a city name");
         });
         it('should return a timestamp corresponding to the time when the iss pass over the given location from a valid city name', async function () {
-            this.timeout(5000);
-            await wait(1000);
+            await wait(1250);
 
             let validCityName = "Houston";
 
